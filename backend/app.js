5c1d5d520e8174cc208885f23aff6b32748e9517
williamDenStore <113104886+williamDenStore@users.noreply.github.com>
2023-10-03 14:49:19 +0200

    create db and table

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,31 @@ const express = require("express");
 const cors = require('cors');
 const bodyParser = require('body-parser')
 const app = express();
-
 const port = 3000;
+var mysql = require('mysql2');
 
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "developer",
+    password: "password",
+    database: "gbgmysql"
+  });
+  
+
+function createtable(){
+    con.connect(function(err) {
+      if (err) throw err;
+      console.log("Connected!"); 
+      var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+      con.query(sql, function (err, result) {
+      if (err) console.warn("table exists");
+      else{
+        console.log("Table created");
+      }
+    });
+    });
+  }
+  createtable();
 
 app.use(cors({
     origin: 'http://localhost:5173',
