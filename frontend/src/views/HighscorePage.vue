ab54b5499bdf4f26ec870e30fa338f29f1f2dfc1
Bob <113605310+BobHogvall@users.noreply.github.com>
2023-10-12 11:54:15 +0200

    Create html code for HighscorePage in vue

---
+++ frontend/src/views/HighscorePage.vue
@@ -0,0 +1,57 @@
+<script setup>
+</script>
+<template>
+<table>
+    <h1>X/X</h1>
+    <button>Försök igen</button>
+    <a href="#">svg-Spårvagn: länk till quiz 1, dialekt</a>
+    <a href="#">svg-Spårvagn: länk till quiz 2, ordvitsar</a>
+    <a href="#">svg-Spårvagn: länk till quiz 3, karta</a>
+    <tr>
+        <td>1</td>
+        <td>UserNameFromDatabase</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>UserNameFromDatabase</td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>UserNameFromDatabase</td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>UserNameFromDatabase</td>
+    </tr>
+    <tr>
+        <td>5</td>
+        <td>UserNameFromDatabase</td>
+    </tr>
+    <tr>
+        <td>6</td>
+        <td>UserNameFromDatabase</td>
+    </tr>
+    <tr>
+        <td>7</td>
+        <td>UserNameFromDatabase</td>
+    </tr>
+    <tr>
+        <td>8</td>
+        <td>UserNameFromDatabase</td>
+    </tr>
+    <tr>
+        <td>9</td>
+        <td>UserNameFromDatabase</td>
+    </tr>
+    <tr>
+        <td>10</td>
+        <td>UserNameFromDatabase</td>
+    </tr>
+    <tr>
+        <td>11</td>
+        <td>UserNameFromDatabase</td>
+    </tr>
+</table>
+</template>
+<style scoped>
+</style>
