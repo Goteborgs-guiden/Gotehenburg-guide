b21cb7fd44713c8f1107bcef77dfa29f01a4dd62
williamDenStore <113104886+williamDenStore@users.noreply.github.com>
2023-10-17 17:00:33 +0200

    add toplist endpoint

---
+++ backend/routes/highscore.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const express = require("express");
+const database = require("../sql");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const { hashPassword } = require("mysql/lib/protocol/Auth");
+const { use } = require("./register");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const {authenticateToken}=require('./register')
+router.use(cors());
+module.exports=router;
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+router.post("/",authenticateToken,(req,res) => {
+    const body = req.body
+    sql = "SELECT * FROM account WHERE username = '" + req.user.name + "'";
+    database.con.query(sql, (err, result) => {
+        if(err) console.warn("error getting the data");
+        let a = parseInt(result[0]?.highscore,10)
+        let b = parseInt(body.score,10)
+        if(a < b){
+            let sql2 = "UPDATE account SET highscore = " + body.score + " WHERE username = '" + req.user.name + "'";
+            database.con.query(sql2, (err, result) => {
+                if(err) console.warn("error getting the data");
+                res.status(200).send("highscore updated");
+            })
+        }
+        else{
+            res.status(200).send("highscore not updated");
+        }
+    })
+})
+router.get("/toplist", (req, res) => {
+    let sql = "SELECT username, highscore FROM account ORDER BY highscore DESC LIMIT 11";
+    database.con.query(sql, (err, result) => {
+        if(err) console.warn("error getting the data");
+        res.status(200).send(result);
+    })
+})
