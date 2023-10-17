16f3d40c3abc87ca07f93f995459d800afd50b38
williamDenStore <113104886+williamDenStore@users.noreply.github.com>
2023-10-17 16:51:01 +0200

    adds endpoint /score
    the endpoint takes BEARER with accesstoken and a number, if the number is greater than the number stored in the database it store that number instead

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const port = 3000;
 const database = require("./sql")
 const register = require('./routes/register')
 const quiz=require('./routes/quiz')
+const highscore=require('./routes/highscore')
 app.use("/quiz",quiz)
 function createtable(){
     //we should make all tables here!
@@ -25,8 +26,8 @@ function createtable(){
   }
 
 app.use("/quiz", quiz)
-app.use("/register", register)
-
+app.use("/register", register.router)
+app.use("/highscore", highscore)
 app.listen(port, () => {
     database.createtable();
     database.createtableForRegiseter();
