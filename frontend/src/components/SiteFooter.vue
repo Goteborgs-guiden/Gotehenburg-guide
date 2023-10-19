e863efb12ed65b56c57fdca5ddf0f6fef8ef8222
signealfonson <112624460+signealfonson@users.noreply.github.com>
2023-10-19 13:31:51 +0200

    Adds CSS to our footer
    First commit

--- frontend/src/components/SiteFooter.vue
+++ frontend/src/components/SiteFooter.vue
@@ -2,35 +2,32 @@
 
 </script>
 <template>
-   <footer class="footer">
-    <div id="copyright">copyright @ a-laget</div>
-    <div id="contact">contact: a-laget@alaget.se</div>
+   <footer class="mainFooter">
+    <div class ="footerContent">
+      <div id="copyright">copyright @ a-laget</div>
+      <div id="contact">contact: a-laget@alaget.se</div>
+    </div>
   </footer>
 </template>
 <style scoped>
-.footer {
-  fill: #214F75;
-  height: 200px;
-}
 
-#copyright {
+footer{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+.footerContent {
+  display: flex;
+  background-color: #214F75;
+  height: 100px;
+  justify-content: space-evenly;
+  align-items: center;
+  height: 5%;
   color: #FFF;
-font-family: Permanent Marker;
-font-size: 20px;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
+  font-family: Permanent Marker;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
 }
-
-#contact {
-  color: #FFF;
-font-family: Permanent Marker;
-font-size: 20px;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
-}
-
-
-
 </style>
