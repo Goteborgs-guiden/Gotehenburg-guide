02ab1b843c8032f1ec74b558a0b54a91f41362fe
williamDenStore <113104886+williamDenStore@users.noreply.github.com>
2023-10-19 16:37:13 +0200

    backend for fill in blank quiz

--- backend/routes/quiz.js
+++ backend/routes/quiz.js
@@ -10,7 +10,7 @@ router.use(cors());
 module.exports=router;
 //http endpoints
 router.use(bodyParser.json())
-router.post("/abcanswer/:id",(request,response, next) =>{
+router.post("/abcanswer/:id",(request,response) =>{
     const body = request.body;
     let id = request.params.id;
     let sql = 'SELECT * FROM abcquestion WHERE id = '+id;
@@ -34,7 +34,42 @@ router.get("/abcquestion/:id", (request, response, next) => {
     let sql = 'SELECT * FROM abcquestion WHERE id = '+id;
     database.con.query(sql,(err, result) => {
         if(err) console.warn("error getting the data");
-        response.send
+        if(result[0]?.question){
+            const data = result[0]
+            delete data.correct;
+            
+            response.json(data)
+        }
+        else{
+            response.status(404).send("site not found")
+        }
+    });   
+});
+router.post("/fillblank/:id",(request,response) =>{
+    const body = request.body;
+    let id = request.params.id;
+    let sql = 'SELECT * FROM fillBlank WHERE id = '+id;
+    database.con.query(sql,(err, result) => {
+        if(err) console.warn("error getting the data");
+        if(result[0]?.question){
+            if(body.answer === result[0].correct){
+                response.status(200).send(true);
+            }
+            else{
+                response.status(200).send(false);
+            }
+        }
+        else{
+            response.status(404).send("site not found")
+        }
+    });
+})
+router.get("/fillblank/:id", (request, response, next) => {
+    let id = request.params.id;
+    let sql = 'SELECT * FROM fillBlank WHERE id = '+id;
+    database.con.query(sql,(err, result) => {
+        console.log(result)
+        if(err) console.warn("error getting the data");
         if(result[0]?.question){
             const data = result[0]
             delete data.correct;
