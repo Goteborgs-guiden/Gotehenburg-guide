caa6728c8797be2a2c03e13867f5373e65bb9ee0
williamDenStore <113104886+williamDenStore@users.noreply.github.com>
2023-10-19 16:37:52 +0200

    Update sql.js

--- backend/sql.js
+++ backend/sql.js
@@ -11,7 +11,7 @@ function createtable(){
     con.connect(function(err) {
         if (err) throw err;
         console.log("Connected!");
-        var sql = "CREATE TABLE abcquestion (id INT AUTO_INCREMENT PRIMARY KEY, question VARCHAR(255), alternatives VARCHAR(255), correct VARCHAR(255))";
+        var sql = "CREATE TABLE abcquestion (id INT AUTO_INCREMENT PRIMARY KEY, question VARCHAR(255), alternatives VARCHAR(255), correct VARCHAR(255), img VARCHAR(255))";
         con.query(sql, function (err, result) {
             if (err) console.warn("abc table exists");
             else{
