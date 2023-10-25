31c1e3af711a8c605cafd221001d02aad9e21412
Simon T <simtho0624@gmail.com>
2023-10-25 20:13:53 +0200

    Update the change

--- frontend/src/components/TheHomepage.vue
+++ frontend/src/components/TheHomepage.vue
@@ -3,15 +3,16 @@ import {RouterLink} from "vue-router";
 </script>
 <template>
   <div class="Trams">
-    <RouterLink class="RouterL" to="/tjot">
+    <RouterLink class="RouterLT" to="/tjot">
       <img class="Tramquiz1" src="../assets/img/old tramquiz 1.svg" alt="tramquiz1">
     </RouterLink>
-    <RouterLink class="RouterL" to="/ordvitsknok">
+    <RouterLink class="RouterLT" to="/ordvitsknok">
       <img class="Tramquiz2" src="../assets/img/new tramquiz 2.svg" alt="tramquiz2">
     </RouterLink>
-    <RouterLink class="RouterL" to="/geografikack">
+    <RouterLink class="RouterLT" to="/geografikack">
       <img class="Tramquiz3" src="../assets/img/middle old tramquiz 3.svg" alt="tramquiz3">
     </RouterLink>
+
   </div>
 
 </template>
@@ -30,6 +31,9 @@ import {RouterLink} from "vue-router";
     display: none;
   }
   .Tramquiz3{
+    display: none;
+  }
+  .RouterLT{
     display: none;
   }
 }
@@ -51,3 +55,4 @@ import {RouterLink} from "vue-router";
 }
 
 </style>
+
