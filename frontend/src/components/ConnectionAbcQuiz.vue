12c3017d2b5026d23c12bc112354c93403849325
signealfonson <112624460+signealfonson@users.noreply.github.com>
2023-10-25 11:40:45 +0200

    First commit
    Basic CSS with media query.

--- frontend/src/components/ConnectionAbcQuiz.vue
+++ frontend/src/components/ConnectionAbcQuiz.vue
@@ -2,7 +2,7 @@
 import { onMounted, ref } from 'vue'
 let currentQuestion = ref(0)
 //const apiUrl = "http://127.0.0.1:3000/";
-let abcdata = ref('')
+let question = ref('')
 let alternatives = ref([''])
 let correctData = ref('')
 let onGoingQuiz = true
@@ -26,11 +26,11 @@ function sendAnswer(input, id, answerid) {
       correctData.value = data
       if (data) {
         console.log('answerid=', answerid)
-        document.getElementById('btn' + answerid).style.border = '2px solid green'
+        document.getElementById('btn' + answerid).style.border = '2rem solid green'
         points.value++
       } else {
         console.log('answerid=', answerid)
-        document.getElementById('btn' + answerid).style.border = '2px solid red'
+        document.getElementById('btn' + answerid).style.border = '2rem solid red'
       }
       setTimeout(function () {
         getQuestion(currentQuestion.value++)
@@ -48,7 +48,7 @@ function getQuestion(id) {
       .then((response) => response.json())
       .then((data) => {
         console.log('response from server:', data)
-        abcdata.value = data.question
+        question.value = data.question
         alternatives.value = data.alternatives.split(',')
       })
   } else onGoingQuiz = false
@@ -56,39 +56,28 @@ function getQuestion(id) {
 </script>
 
 <template>
-  <div class="grid-container">
-    <div class="item1" id="question-image">
-      <img src="#" />
-    </div>
-
-    <div v-if="onGoingQuiz" id="abc-quiz" class="item2">
-      <p>{{ abcdata }}</p>
+  <div class="quiz-container">
+    <div v-if="onGoingQuiz" id="abc-quiz">
+      <div id="question">
+        {{ question }}
+          </div>
       <div class="selection">
-        <from>
-          <button id="btn0" @click="sendAnswer(alternatives[0], currentQuestion, 0)">
+          <button class="btn" @click="sendAnswer(alternatives[0], currentQuestion, 0)">
             {{ alternatives[0] }}
           </button>
-          <button id="btn1" @click="sendAnswer(alternatives[1], currentQuestion, 1)">
+          <button class="btn" @click="sendAnswer(alternatives[1], currentQuestion, 1)">
             {{ alternatives[1] }}
           </button>
-          <button id="btn2" @click="sendAnswer(alternatives[2], currentQuestion, 2)">
+          <button class="btn" @click="sendAnswer(alternatives[2], currentQuestion, 2)">
             {{ alternatives[2] }}
           </button>
-          <button id="btn3" @click="sendAnswer(alternatives[3], currentQuestion, 3)">
+          <button class="btn" @click="sendAnswer(alternatives[3], currentQuestion, 3)">
             {{ alternatives[3] }}
           </button>
-        </from>
+        
       </div>
     </div>
-    <div class="item3">
-      <button id="visible-from-second-question" class="one">Tillbaka</button>
-    </div>
-    <div class="item4">
-      <button id="visible-from-first-to-second-to-last-question" class="one two">Nästa</button>
-    </div>
-  </div>
   <div>
-    <from> </from>
   </div>
   <div v-if="onGoingQuiz">
     <p v-if="correctData">Rätt svar!</p>
@@ -99,95 +88,94 @@ function getQuestion(id) {
     <p v-else>Rackarns rabarber det där gick inte så bra!</p>
     <p>{{ points }} Poäng</p>
   </div>
+  </div>
 </template>
 <style scoped>
-.item1 {
-  grid-area: image;
-}
-.item2 {
-  grid-area: choices;
-}
-.item3 {
-  grid-area: buttonOne;
-}
-.item4 {
-  grid-area: buttonTwo;
-}
-.grid-container {
-  display: grid;
-  grid-template-columns: 1fr 50% 1fr;
-  grid-template-rows: 1fr 1fr 20%;
-  grid-template-areas:
-    '. image .'
-    '. choices .'
-    'buttonOne . buttonTwo';
+
+.quiz-container {
+  height: 100vh;
+  width: 100vw;
 }
 
-.item1 {
-  justify-self: center;
+#abc-quiz {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 }
 
-.item2 {
-  justify-self: center;
+#question {
+  border-radius: 25px;
+border: 4px solid #406C90;
+background: rgba(232, 243, 253, 0.91);
+box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+width: 40%;
+height: 7rem;
+flex-shrink: 0;
+color: #000;
+font-family: Newsreader;
+font-size: 2rem;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+display: flex;
+justify-content: center;
+align-items: center;
+margin-top: 2rem;
+margin-bottom: 2rem;
 }
 .selection {
   text-align: center;
-  width: 59.53125rem;
-  height: 13.6875rem;
+  width: 40%;
   background-color: rgba(64, 108, 144, 0.9);
   border-radius: 0.8rem;
+  height: 20rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  padding-top: 0.8rem;
+  padding-bottom: 0.8rem;
 }
-#btn0 {
-  width: 23.25rem;
-  height: 3.5rem;
-  border-radius: 1.90625rem;
-  border: 2px solid #214f75;
-  background: #e8f3fd;
-  margin-top: 2rem;
-  margin-right: 5.5rem;
-}
-#btn1 {
-  width: 23.25rem;
-  height: 3.5rem;
-  border-radius: 1.90625rem;
-  border: 2px solid #214f75;
-  background: #e8f3fd;
-}
-#btn2 {
-  width: 23.25rem;
-  height: 3.5rem;
-  border-radius: 1.90625rem;
-  border: 2px solid #214f75;
-  background: #e8f3fd;
-  margin-top: 2.5rem;
-  margin-right: 5.5rem;
-}
-#btn3 {
-  width: 23.25rem;
+.btn {
+  width: 70%;
   height: 3.5rem;
   border-radius: 1.90625rem;
   border: 2px solid #214f75;
   background: #e8f3fd;
 }
 
-.item3 {
-  justify-self: center;
+@media screen and (max-width: 768px) {
+  #question {
+    width: 80%;
+  }
+  .quiz-container {
+  height: 100vh;
+  width: 100%;
 }
-.item4 {
-  justify-self: center;
-}
-.one {
-  padding: 0.6rem 1.4rem;
-  border-radius: 0.8rem;
-  border: 5px solid #91b6d8;
-  background: #fff;
-  color: #214f75;
-  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.3);
-  font-family: 'Newsreader';
-  font-size: 1.3125rem;
-}
-
-.two {
-  padding: 0.6rem 2rem;
+  #abc-quiz {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+  }
+  .question {
+    display: flex;
+    flex-direction: column;
+  }
+  .selection {
+    width: 90%;
+    height: 20rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 0.8rem;
+    padding-bottom: 0.8rem;
+  }
+  .btn {
+    width: 70%;
+    margin: 0.8rem;
+  }
+ 
 }
 </style>
