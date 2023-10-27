7cb93f8055c539c4fb6e16087124a7cbbc7753c1
Viktormes <112468964+Viktormes@users.noreply.github.com>
2023-10-27 10:30:48 +0200

    move pictures to the public map
    Also changed the path for the trams everywhere in the code.

--- frontend/src/components/TheHomepage.vue
+++ frontend/src/components/TheHomepage.vue
@@ -4,13 +4,13 @@ import {RouterLink} from "vue-router";
 <template>
   <div>
     <RouterLink class="RouterL" to="/tjot">
-      <img src="../assets/img/old tramquiz 1.svg" alt="tramquiz1">
+      <img src="/old tramquiz 1.svg" alt="tramquiz1">
     </RouterLink>
     <RouterLink class="RouterL" to="/ordvitsknok">
-      <img src="../assets/img/new tramquiz 2.svg" alt="tramquiz2">
+      <img src="/new tramquiz 2.svg" alt="tramquiz2">
     </RouterLink>
     <RouterLink class="RouterL" to="/geografikack">
-      <img src="../assets/img/middle old tramquiz 3.svg" alt="tramquiz3">
+      <img src="/middle old tramquiz 3.svg" alt="tramquiz3">
     </RouterLink>
   </div>
   <p>
