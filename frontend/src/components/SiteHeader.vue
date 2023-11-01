a00f54fe30fb13736b7592dffa0639ddadc405bb
williamDenStore <113104886+williamDenStore@users.noreply.github.com>
2023-11-01 11:23:15 +0100

    create add friend functionallity

--- frontend/src/components/SiteHeader.vue
+++ frontend/src/components/SiteHeader.vue
@@ -9,6 +9,7 @@ const windowWidth = ref()
 const dialogs = useDialogStore()
 const userInfo = ref('');
 const isLoggedin = ref(false);
+const friend = ref('');
 onMounted(() => {
   getInfo();
 })
@@ -47,24 +48,25 @@ function getInfo() {
       mobileNav.value = false;
       return;
     }
-function reload(){
-  const time = Date.parse(localStorage.getItem('time'));
-  console.log("time",time)
-  const currentTime = new Date(Date.now());
-    const timeLeft = time - currentTime;
-    console.log("time",time)
-    console.log("currentTime",currentTime);
-    console.log("time left",timeLeft)
-  if(timeLeft > 0){
-    console.log("time left",timeLeft)
-    setTimeout(function(){location.reload();}, timeLeft)
-  }
-  
-}
 function logout(){
   localStorage.removeItem('accessToken');
   localStorage.removeItem('time');
   location.reload();
+}
+function addfriend(){
+  console.log("friend",friend.value)
+  fetch('http://127.0.0.1:3000/user/friend', {
+    method: 'POST',
+    body: JSON.stringify({friend:friend.value}),
+    headers: {
+      'Content-type': 'application/json',
+      'Authorization': 'BEARER ' + localStorage.getItem('accessToken')
+    }
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('response from server:', data)
+    })
 }
 </script>
 
@@ -81,7 +83,6 @@ function logout(){
             <div v-else>
                 <RouterLink class="navitem" style="text-decoration: none;" to="/profile">{{userInfo.username}}</RouterLink>
                 <button class="button" @click="logout()">logout</button>
-                <div v-if="reload()"></div>
             </div>
         </div>
     <nav class="navbar">
@@ -101,7 +102,8 @@ function logout(){
         </li>
         <li><RouterLink class="navitem" to="/highscore">Highscore</RouterLink></li>
         <li><RouterLink class="navitem" to="/gbguide">GBGuide</RouterLink></li>
-        <li><input id="search"  placeholder="Hitta vänner"></li>
+        <li><input id="search" v-model="friend" placeholder="Lägg till vän"></li>
+        <button class="button" @click="addfriend()">add</button>
       </ul>  
       <div class="icon">
         <i @click="toggleMobileNav" v-show="mobile" class="far fa-bars" :class="{ 'icon-active': mobileNav }"></i>
@@ -126,8 +128,8 @@ function logout(){
         <div v-else>
           <RouterLink class="navitem" to="/profile">Min Profil</RouterLink>
         </div>
-        <input id="search"  placeholder="Hitta vänner">
-
+        <input id="search" v-model="friend" placeholder="Lägg till vän">
+        <button class="button" @click="addfriend()">add</button>
       </ul>
       </Transition>
   </nav>
