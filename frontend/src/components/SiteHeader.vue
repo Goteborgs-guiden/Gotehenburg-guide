cc8b2ac25fc21ecad2143fbd2469a3b8e52e837c
williamDenStore <113104886+williamDenStore@users.noreply.github.com>
2023-11-01 12:22:03 +0100

    fix check if user is logged in

--- frontend/src/components/SiteHeader.vue
+++ frontend/src/components/SiteHeader.vue
@@ -8,7 +8,6 @@ const mobileNav = ref()
 const windowWidth = ref()
 const dialogs = useDialogStore()
 const userInfo = ref('');
-const isLoggedin = ref(false);
 onMounted(() => {
   getInfo();
 })
@@ -26,12 +25,6 @@ function getInfo() {
       .then((data) => {
         userInfo.value = data;
         console.log('response from server:', data)
-        if(data){
-          isLoggedin.value = true;
-        }
-        else{
-          isLoggedin.value = false;
-        }
       })
     }
     function toggleMobileNav(){
@@ -47,10 +40,29 @@ function getInfo() {
       mobileNav.value = false;
       return;
     }
+function reload(){
+
+  const time = Date.parse(localStorage.getItem('time'));
+  if(!time){
+    return false;
+  }
+  const currentTime = new Date(Date.now());
+  const timeLeft = time - currentTime;
+  console.log("time",time)
+  console.log("currentTime",currentTime);
+  console.log("time left",timeLeft)
+  setTimeout(function(){location.reload();}, timeLeft)
+}
 function logout(){
   localStorage.removeItem('accessToken');
   localStorage.removeItem('time');
   location.reload();
+}
+function isLoggedIn(){
+  if(localStorage.getItem('accessToken')){
+    return true;
+  }
+  return false;
 }
 </script>
 
@@ -60,13 +72,14 @@ function logout(){
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <div id="headerContent">
       <RouterLink id="GBGlogo" to="/">GBGuiden</RouterLink>
-          <div v-if="!isLoggedin && !mobile">
+          <div v-if="!isLoggedIn() && !mobile">
                 <button class="button" @click="dialogs.toggleLogin">Logga in</button>
                 <button class="button" @click="dialogs.toggleRegister">Registrera dig</button>
             </div>
-            <div v-else>
+            <div v-if="isLoggedIn()">
                 <RouterLink class="navitem" style="text-decoration: none;" to="/profile">{{userInfo.username}}</RouterLink>
                 <button class="button" @click="logout()">logout</button>
+                <div v-if="reload()"></div>
             </div>
         </div>
     <nav class="navbar">
@@ -99,18 +112,15 @@ function logout(){
         <RouterLink class="dropdownitem" to="/geografikack">Geografikäck</RouterLink>
         <RouterLink class="navitem" to="/highscore">Highscore</RouterLink>
         <RouterLink class="navitem" to="/gbguide">GBGuide</RouterLink>
-<div v-if="isLoggedIn">
-<div v-if="isLoggedIn">
+<div v-if="isLoggedIn()">
+<div v-if="isLoggedIn()">
         <RouterLink class="navitem" to="/profile">Min Profil</RouterLink>
 </div>
 </div>
-        <div v-if="!isLoggedIn" class="login-and-register">
+        <div v-if="!isLoggedIn()" class="login-and-register">
               <button class="button" @click="dialogs.toggleLogin">Logga in</button>
               <button class="button" @click="dialogs.toggleRegister">Registrera dig</button>
             </div>
-        <div v-else>
-          <RouterLink class="navitem" to="/profile">Min Profil</RouterLink>
-        </div>
         <input id="search"  placeholder="Hitta vänner">
 
       </ul>
